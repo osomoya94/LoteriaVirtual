0b45c0d77afe08fa6e6bd3116540b5c59cc3d7f8
emanuelmoya94 <emanuelmoya11@gmail.com>
2026-04-22 12:24:27 -0300

    feat: implementacion modulo cartones, baja logica de sorteos y endpoints completados

---
+++ Loteria.Entidades/DTOs/UsuarioResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using Loteria.Entidades.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loteria.Entidades.DTOs
+{
+    public class UsuarioResponseDTO
+    {
+        public int Id { get; set; }
+        public int RolId { get; set; }
+        public required string Username { get; set; }
+        public bool Activo { get; set; } 
+    }
+}
