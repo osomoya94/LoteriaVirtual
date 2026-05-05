6c4751e0517b8d19c1a2511a1da81ed4d0700723
furiacalchaqui <erikammn@hotmail.com>
2026-05-05 16:21:10 -0300

    .

---
+++ Loteria.Entidades/Pago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loteria.Entidades
+{ public class Pago
+        {
+            public int Id { get; set; }
+            public string DniJugador { get; set; } = string.Empty;
+            public decimal Monto { get; set; }
+            public DateTime Fecha { get; set; }
+            public string Estado { get; set; } = "Pendiente"; // Valor por defecto
+      
+    }
+}
---
+++ Loteria.Entidades/Sorteo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loteria.Entidades
+{
+    public class Sorteo
+    {
+            public int Id { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+            public DateTime Fecha { get; set; }
+            public decimal Precio { get; set; }
+            public string ModoExtraccion { get; set; } = string.Empty;
+
+}
+}
