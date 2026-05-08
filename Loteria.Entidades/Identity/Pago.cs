d37a9adb3df5474655b54d538e57b701aa64a27c
furiacalchaqui <erikammn@hotmail.com>
2026-05-08 13:33:28 -0300

    hasta aca llegue :)

--- Loteria.Entidades/Identity/Pago.cs
+++ Loteria.Entidades/Identity/Pago.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Loteria.Entidades
+namespace Loteria.Entidades.Identity
 { public class Pago
         {
             public int Id { get; set; }
